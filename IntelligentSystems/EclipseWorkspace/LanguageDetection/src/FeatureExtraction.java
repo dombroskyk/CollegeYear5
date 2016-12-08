9a6f45e3a408240dfb3fb83565f32e8b90063a18
Kevin Dombrosky <dombroskyk@gmail.com>
2016-12-07 19:25:48 -0500

    Initial work for mdoel generation

--- IntelligentSystems/EclipseWorkspace/LanguageDetection/src/FeatureExtraction.java
+++ IntelligentSystems/EclipseWorkspace/LanguageDetection/src/FeatureExtraction.java
@@ -1,21 +1,20 @@
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 
-import jAudioFeatureExtractor.ACE.DataTypes.FeatureDefinition;
 import jAudioFeatureExtractor.AudioFeatures.MFCC;
 import jAudioFeatureExtractor.AudioFeatures.MagnitudeSpectrum;
 import jAudioFeatureExtractor.jAudioTools.AudioSamples;
 
 public class FeatureExtraction {
 	
-	public static void main(String[] args){
-		
-		final double SAMPLE_DURATION = 10.0d;
+	final static double SAMPLE_DURATION = 3.0d;
+	
+	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 		
 		if( args.length == 0 ){
 			System.err.println( "Please supply the local path to audio files to train on" );
@@ -23,9 +22,27 @@ public class FeatureExtraction {
 		}
 		// filter non .wav files
 		
-		//AudioSamples[] audioSamples = new AudioSamples[args.length - 1];
+		PrintWriter enWriter = new PrintWriter("en.csv", "UTF-8");
+		PrintWriter esWriter = new PrintWriter("es.csv", "UTF-8");
+		PrintWriter plWriter = new PrintWriter("pl.csv", "UTF-8");
 		for( int i = 0; i < args.length; i++ ){
 			String uid = args[i].substring(0, args[i].length()-4);
+			String langAbbr = args[i].substring(0, 2);
+			PrintWriter writer;
+			switch(langAbbr){
+				case "en":
+					writer = enWriter;
+					break;
+				case "es":
+					writer = esWriter;
+					break;
+				case "pl":
+					writer = plWriter;
+					break;
+				default:
+					writer = null;
+					break;
+			}
 			File inputFile = new File(args[i]);
 			AudioSamples audioSample;
 			double[][] samples;
@@ -42,34 +59,27 @@ public class FeatureExtraction {
 
 			try{
 				double sampleRate = audioSample.getSamplingRateAsDouble();
-				PrintWriter writer = new PrintWriter(uid+".csv", "UTF-8");
 				for( double[] sample : samples ){
 					MagnitudeSpectrum currMagSpec = new MagnitudeSpectrum();
 					MFCC currMFCC = new MFCC();
 					double[][] mfccMagSpecPass = new double[1][];
 					mfccMagSpecPass[0] = currMagSpec.extractFeature(sample, sampleRate, null);
 					double[] mfccSamples = currMFCC.extractFeature(sample, sampleRate, mfccMagSpecPass);
-					System.out.println(mfccSamples.length);
-					for(double coef : mfccSamples){
-						System.out.println(coef);
-					}
-					
 					
 					StringBuilder result = new StringBuilder();
 				    for(double mfcc : mfccSamples ){
 				        result.append(Double.toString(mfcc) + ",");
 				    }
-				    writer.println(result.substring(0, result.length() - 1));
+				    writer.println(result);
 				}
-			    writer.close();
 			}catch(Exception e){
 				e.printStackTrace();
 				System.exit(3);
 			}
 		}
-		
-		
-		
+		enWriter.close();
+		esWriter.close();
+		plWriter.close();
 	}
 	
 }
